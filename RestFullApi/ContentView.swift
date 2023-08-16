b52f67cbb80cba6ca258ae8c04b7754abd64b9f7
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-08-17 01:25:04 +0300

    Initial Commit

---
+++ RestFullApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestFullApi
+//
+//  Created by ceksi on 17.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestFullApi/RestFullApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestFullApiApp.swift
+//  RestFullApi
+//
+//  Created by ceksi on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestFullApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
